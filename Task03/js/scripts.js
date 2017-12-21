dfed46b19664bfdc039d02dd55c7218fcee6ef85
wooqash <l.sobola@hypermediaisobar.pl>
2017-12-21 17:50:26 +0100

    Refactored code for task03

--- Task03/js/scripts.js
+++ Task03/js/scripts.js
@@ -49,60 +49,120 @@
 			random: random
 		};
 	})();
+	const showErrors = function(error){
+		console.error(error.name, error.message);
+	};
+	const noItemException = function(id){
+		this.message = 'The element with ID: ' + id + ' doesn\'t exists!';
+		this.name = 'noItemException: ';
+	};
+	const invalidMsg = function(msg){
+		this.nextElementSibling.textContent = msg || '';
+	};
 	const showResults = function(val1, val2){
-		results.classList.remove('show');
-		if(!val1){
-			numb1.nextElementSibling.textContent = 'Error: Type a number';
-		}
-		else{
-			numb1.nextElementSibling.textContent = '';
-		}
-		if(!val2){
-			if(val2 !== 0){				
-				numb2.nextElementSibling.textContent = 'Error: Type a number';
+		try{
+			// List of test which checks that fileds exists
+			if(!results){
+				throw new noItemException('Results');
+			}
+			if(!numb1){
+				throw new noItemException('Numb1');
+			}
+			if(!numb2){
+				throw new noItemException('Numb2');
+			}
+			if(!addition){
+				throw new noItemException('ResultOfAdd');
+			}
+			if(!substraction){
+				throw new noItemException('ResultOfSub');
+			}
+			if(!multiplication){
+				throw new noItemException('ResultOfMuli');
+			}
+			if(!division){
+				throw new noItemException('ResultOfDiv');
+			}
+			if(!restFromDivision){
+				throw new noItemException('ResultOfMod');
+			}
+			
+			results.classList.remove('show');
+			if(!val1){
+				invalidMsg.call(numb1, 'Error: Type a number');
 			}
 			else{
-				numb2.nextElementSibling.textContent = 'Error: Type a number other than 0';
+				invalidMsg.call(numb1);
 			}
-			return;
+			
+			if(!val2){
+				if(val2 !== 0){				
+					invalidMsg.call(numb2, 'Error: Type a number');
+				}
+				else{
+					invalidMsg.call(numb2, 'Error: Type a number other than 0');
+				}
+				return;
+			}
+			else{
+				invalidMsg.call(numb2);
+			}
+
+
+			if(typeof(val1) === 'number' && typeof(val2) === 'number'){
+				results.classList.add('show');
+				addition.textContent = calc.add(val1, val2);
+				substraction.textContent = calc.sub(val1, val2);
+				multiplication.textContent = calc.multi(val1, val2);
+				division.textContent = calc.div(val1, val2);
+				restFromDivision.textContent = calc.mod(val1, val2);
+			}
+			else{
+				addition.textContent = '';
+				substraction.textContent = '';
+				multiplication.textContent = '';
+				division.textContent = '';
+				restFromDivision.textContent = '';
+			}
 		}
-		else{
-			numb2.nextElementSibling.textContent = '';
-		}
-		
-		if(typeof(val1) === 'number' && typeof(val2) === 'number'){
-			results.classList.add('show');
-			addition.textContent = calc.add(val1, val2);
-			substraction.textContent = calc.sub(val1, val2);
-			multiplication.textContent = calc.multi(val1, val2);
-			division.textContent = calc.div(val1, val2);
-			restFromDivision.textContent = calc.mod(val1, val2);
-		}
-		else{
-//			results.classList.remove('show');
-			addition.textContent = '';
-			substraction.textContent = '';
-			multiplication.textContent = '';
-			division.textContent = '';
-			restFromDivision.textContent = '';
-		}
+		catch (e) {
+			showErrors(e);
+		}	
 	};
 	
-	okBtn.addEventListener('click', function(evt){
-		evt.preventDefault();
-		var val1 = parseFloat(numb1.value);
-		var val2 = parseFloat(numb2.value);
-				
-		showResults(val1, val2);
-	});
-	randomBtn.addEventListener('click', function(evt){
-		evt.preventDefault();
-		var val1 = calc.random(1,20);
-		var val2 = calc.random(1,20);
-		
-		numb1.value = val1;
-		numb2.value = val2;
+	try{
+		if(okBtn){
+			okBtn.addEventListener('click', function(evt){
+				evt.preventDefault();
+				var val1 = numb1 ? parseFloat(numb1.value) : null;
+				var val2 = numb2 ? parseFloat(numb2.value) : null;
 
-		showResults(val1, val2);
-	});
+				showResults(val1, val2);
+			});
+		}
+		else{
+			throw new noItemException('OkBtn');
+		}
+
+		if(randomBtn){
+			randomBtn.addEventListener('click', function(evt){
+				evt.preventDefault();
+				var val1 = calc.random(1,20);
+				var val2 = calc.random(1,20);
+
+				numb1.value = val1;
+				numb2.value = val2;
+
+				showResults(val1, val2);
+			});
+		}
+		else{
+			throw new noItemException('RandomBtn');
+		}
+	}
+	catch (err) {
+		showErrors(err);
+	}
+	
+	
 })();
